fd43c4a1b4f893ef04c1d5584be397b237a9927d
Tilman Buchner <tilman.buchner@gmail.com>
2013-07-12 06:17:21 +0000

    undo

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var fs = require('fs');
-//var buf = fs.readFileSync("index.html");
+var fs = require('fs');
+var buf = fs.readFileSync("index.html");
 //var content = buf.toString("utf8",0,buf.length); 
 
 app.get('/', function(request, response) {
